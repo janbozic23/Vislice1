d9e228108a4921a5a55b3440b51cf613a9c7a578
janbozic23 <49915504+janbozic23@users.noreply.github.com>
2019-05-13 12:38:51 +0200

    Add files via upload
    Problem je bil z githubom, zato sem po navodilu asistenta datoteko naložil ročno.

---
+++ model.py
@@ -0,0 +1,86 @@
+STEVILO_DOVOLJENIH_NAPAK = 10 
+PRAVILNA_CRKA = '+'
+PONOVLJENA_CRKA = 'o'
+NAPACNA_CRKA = '-'
+ZMAGA = 'W'
+PORAZ = 'X'
+
+class Igra:
+
+    def __init__(self, beseda, crke = None):
+        geslo = beseda
+        if crke = None:
+            self.crke = []
+        else:
+            self.crka = crke
+    
+    def napacne_crke(self):
+        napacne_crke = []
+        for crka in self.crke:
+            if crka not in self.geslo:
+                napacne_crke.append(crka)
+        return napacne_crke
+
+    def pravilne_crke(self):
+        pravilne_crke = []
+        for crka in self.crke:
+            if crka in self.geslo:
+                pravilne_crke.append(crka)
+        return pravilne_crke
+
+    def stevilo_napak(self):
+        return len(self.napacne_crke)
+
+    def zmaga(self):
+        for crka in self.geslo:
+            if crka not in self.pravilne_crke:
+                return False
+            else:
+                return True
+
+    def poraz(self):
+        if self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK:
+            return False
+        else:
+            return True
+
+    def pravilni_del_gesla(self):
+        pravilni_del = geslo
+        for crka in geslo:
+            if crka not in self.pravilne_crke:
+                pravilni_del = geslo.replace().replace(crka, "_")
+        return pravilni_del
+
+    def nepravilni_ugibi(self):
+        nepravilni_ugibi = ""
+        for crka in self.napacne_crke():
+            nepravilni_ugibi += " " + crka
+        return nepravilni_ugibi
+
+    def ugibaj(self):
+        velika_crka = crka.upper()
+        if velika_crka in self.crke:
+            return PONOVLJENA_CRKA
+        elif velika_crka in self.geslo:
+            if self.zmaga():
+                return ZMAGA
+            else:
+                return PRAVILNA_CRKA
+        elif velika_crka not in self.geslo:
+            if self.poraz():
+                return PORAZ
+            else:
+                return NAPACNA_CRKA
+            
+
+with open(besede.txt, "r", encoding = "utf-8") as besede:
+    bazen_besed = []
+    besede.read()
+    for vrstica in besede:
+        bazen_besed.append(vrstica)
+    return bazen_besed
+
+from random import *
+
+def nova_igra(Igra):
+    return Igra(bazen_besed.choice())
